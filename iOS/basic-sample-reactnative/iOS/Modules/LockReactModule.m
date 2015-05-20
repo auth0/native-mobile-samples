b8081e44e23e190a77747b5faaa90df6015eea3a
Hernan Zalazar <hernanzalazar@gmail.com>
2015-05-20 10:19:31 -0300

    Update RN sample

--- iOS/basic-sample-reactnative/iOS/Modules/LockReactModule.m
+++ iOS/basic-sample-reactnative/iOS/Modules/LockReactModule.m
@@ -21,11 +21,57 @@
 // THE SOFTWARE.
 
 #import "LockReactModule.h"
-#import <LockReact/A0LockReact.h>
+#import "A0LockReact.h"
+#import <Lock/A0IdentityProviderAuthenticator.h>
+
+#if __has_include(<Lock-Facebook/A0FacebookAuthenticator.h>)
+#define FACEBOOK_ENABLED 1
+#import <Lock-Facebook/A0FacebookAuthenticator.h>
+#endif
+
+#if __has_include(<Lock-Twitter/A0TwitterAuthenticator.h>)
+#define TWITTER_ENABLED 1
+#import <Lock-Twitter/A0TwitterAuthenticator.h>
+#endif
+
+#if __has_include(<Lock-GooglePlus/A0GooglePlusAuthenticator.h>)
+#define GOOGLE_PLUS_ENABLED 1
+#import <Lock-GooglePlus/A0GooglePlusAuthenticator.h>
+#endif
 
 @implementation LockReactModule
 
-RCT_EXPORT_MODULE();
+RCT_EXPORT_MODULE(LockReact);
+
+RCT_EXPORT_METHOD(registerNativeAuthentication:(NSArray *)authentications) {
+  A0IdentityProviderAuthenticator *authenticator = [A0IdentityProviderAuthenticator sharedInstance];
+  for (NSDictionary *authentication in authentications) {
+    NSString *name = authentication[@"provider"];
+#ifdef FACEBOOK_ENABLED
+    if ([@"facebook" isEqualToString:name]) {
+      NSArray *permissions = authentication[@"permissions"];
+      if (permissions.count == 0) {
+        permissions = nil;
+      }
+      [authenticator registerAuthenticationProvider:[A0FacebookAuthenticator newAuthenticatorWithPermissions:permissions]];
+    }
+#endif
+#ifdef TWITTER_ENABLED
+    if ([@"twitter" isEqualToString:name]) {
+      NSString *apiKey = authentication[@"api_key"];
+      NSString *apiSecret = authentication[@"api_secret"];
+      [authenticator registerAuthenticationProvider:[A0TwitterAuthenticator newAuthenticatorWithKey:apiKey andSecret:apiSecret]];
+    }
+#endif
+#ifdef GOOGLE_PLUS_ENABLED
+    if ([@"google+" isEqualToString:name]) {
+      NSString *clientId = authentication[@"client_id"];
+      NSArray *scopes = authentication[@"scopes"];
+      [authenticator registerAuthenticationProvider:[A0GooglePlusAuthenticator newAuthenticatorWithClientId:clientId andScopes:scopes]];
+    }
+#endif
+  }
+}
 
 RCT_EXPORT_METHOD(show:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback) {
   dispatch_async(dispatch_get_main_queue(), ^{
