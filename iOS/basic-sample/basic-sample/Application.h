2593968325a6c0be959064d6f646a2c4f1252ba8
Hernan Zalazar <hzalaz@users.noreply.github.com>
2015-07-20 13:52:48 -0300

    Update Application.h

--- iOS/basic-sample/basic-sample/Application.h
+++ iOS/basic-sample/basic-sample/Application.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <foundation/Foundation.h>
+#import <Foundation/Foundation.h>
 
 @class A0SimpleKeychain, A0Lock;
 
